2b6bf75554eaa021a188a812feb5563fa6cf0e84
sneha-amruth <sneha.amruth24@gmail.com>
2024-09-01 16:05:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MainContent from "./components/MainContent";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>What is "GitHub Actions"?</h1>
+      </header>
+      <MainContent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HelpArea.jsx
@@ -0,0 +1,14 @@
+import "./HelpArea.css";
+
+function HelpArea() {
+  return (
+    <section data-testid="help-area" id="help-area">
+      <p>
+        GitHub action is a workflow automation service by GitHub. Automate all
+        kinds of repository related processes and actions.
+      </p>
+    </section>
+  );
+}
+
+export default HelpArea;
